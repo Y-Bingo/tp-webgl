42022715c3ff6ad5826a337102de728551cd0fb4
y_bingo <y_bingo@163.com>
2023-06-24 19:48:45 +0800

    feat: 缩放矩阵

--- mat.js
+++ mat.js
@@ -6,3 +6,14 @@ function createTranslateMatrix(x = 0, y = 0, z = 0) {
         x, y, z, 1.0
     ]);
 }
+
+function createScaleMatrix(x = 0, y = 0, z = 0) {
+	return new Float32Array([
+        x, 0.0, 0.0, 0.0,
+        0.0, y, 0.0, 0.0,
+        0.0, 0.0, z, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ]);
+}
+
+
