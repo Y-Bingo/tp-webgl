05bf95d5ad27eb8fbabcf3c932b474934eb4f768
y_bingo <y_bingo@163.com>
2023-06-24 19:44:32 +0800

    feat: 平移举证

---
+++ mat.js
@@ -0,0 +1,8 @@
+function createTranslateMatrix(x = 0, y = 0, z = 0) {
+	return new Float32Array([
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        x, y, z, 1.0
+    ]);
+}
