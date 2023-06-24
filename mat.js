4f08be0af2a66f4158e19c88378cd4f326ce55f2
y_bingo <y_bingo@163.com>
2023-06-24 20:00:28 +0800

    feat: 旋转矩阵

--- mat.js
+++ mat.js
@@ -1,3 +1,10 @@
+/**
+ * 创建平移矩阵
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} z 
+ * @returns 
+ */
 function createTranslateMatrix(x = 0, y = 0, z = 0) {
 	return new Float32Array([
         1.0, 0.0, 0.0, 0.0,
@@ -7,12 +14,35 @@ function createTranslateMatrix(x = 0, y = 0, z = 0) {
     ]);
 }
 
+/**
+ * 创建缩放矩阵
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} z 
+ * @returns 
+ */
 function createScaleMatrix(x = 0, y = 0, z = 0) {
 	return new Float32Array([
         x, 0.0, 0.0, 0.0,
         0.0, y, 0.0, 0.0,
         0.0, 0.0, z, 0.0,
         0.0, 0.0, 0.0, 1.0
+    ]);
+}
+
+/**
+ * 创建绕 Z 轴旋转的矩阵
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} z 
+ * @returns 
+ */
+function createRotateMatrix( deg ) {
+	return new Float32Array([
+        Math.cos(deg),  -Math.sin(deg), 0.0, 0.0,
+        Math.sin(deg), Math.cos(deg), 0.0, 0.0,
+        0.0,            0.0,           1.0, 0.0,
+        0.0,            0.0,           0.0, 1.0
     ]);
 }
 
