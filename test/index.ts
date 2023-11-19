16399e95445f72dcae480e47ba27b689b61cb306
y_bingo <y_bingo@163.com>
2023-11-19 16:53:17 +0800

    feat: test

--- test/index.ts
+++ test/index.ts
@@ -2,17 +2,21 @@ import { createProgram, createShader } from './game_helper.ts';
 
 const VERTEX_SHADER_SOURCE = `
     attribute vec4 aPosition;
+    attribute float aPointSize;
+    varying vec4 vColor;
     void main() {
         gl_Position = aPosition;
-        gl_PointSize = 10.0;
+        gl_PointSize = aPointSize;
+        vColor = aPosition;
     }
 `;
 const FRAGMENT_SHADER_SOURCE = `
     precision mediump float;
     uniform vec3 uColor;
     uniform float uAlpha;
+    varying vec4 vColor;
     void main() {
-        gl_FragColor = vec4( uColor, uAlpha );
+        gl_FragColor = vColor;
     }
 `;
 
@@ -25,6 +29,7 @@ const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FRAGMENT_SHADER_SOUR
 const program = createProgram(gl, vertexShader, fragmentShader);
 
 const aPosition = gl.getAttribLocation(program, 'aPosition');
+const aPointSize = gl.getAttribLocation(program, 'aPointSize');
 
 const uColor = gl.getUniformLocation(program, 'uColor');
 const uAlpha = gl.getUniformLocation(program, 'uAlpha');
@@ -32,17 +37,25 @@ gl.uniform1f(uAlpha, 1.0);
 gl.uniform3f(uColor, 1.0, 0.4, 0.2);
 
 const POINTS = new Float32Array([
-	0.5,
-	0.5, //
-	-0.5,
-	0.5, //
-	0.5,
-	-0.5,
+	0.5, // x
+	0.5, // y
+	10.0, // pSize
+	-0.5, // x
+	0.5, // y
+	20.0, // pSize
+	0.5, // x
+	-0.5, // y
+	30.0, // pSize
 ]);
+const BYTE = POINTS.BYTES_PER_ELEMENT;
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER, POINTS, gl.STATIC_DRAW);
-gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+// point
+gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, BYTE * 3, 0);
 gl.enableVertexAttribArray(aPosition);
+// pointSize
+gl.vertexAttribPointer(aPointSize, 1, gl.FLOAT, false, BYTE * 3, BYTE * 2);
+gl.enableVertexAttribArray(aPointSize);
 
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.drawArrays(gl.POINTS, 0, 3);
